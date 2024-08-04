4075bf417cac4185a1a5f8b0eed8c96a18c0622f
jathin <jathin176@gmail.com>
2024-08-04 18:23:14 +0530

    post 9, 10 ,11 ,

--- post/forms.py
+++ post/forms.py
@@ -5,11 +5,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model=Post
         
-        fields=('title','title_tag', 'author', 'body',)
+        fields=('title','title_tag', 'body',)
         widgets={
             'title':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Name your title'}),
             'title_tag':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Name your title_tag'}),
-            'author':forms.Select(attrs={'class':'form-control'}),
+            
             'body':forms.Textarea(attrs={'class':'form-control', 'placeholder':'Enter Text here'}),
         }
 
--- post/views.py
+++ post/views.py
@@ -3,6 +3,9 @@ from django.views.generic import ListView , DetailView, CreateView, UpdateView, 
 from .models import *
 from django.urls import path, reverse
 from .forms import PostForm, UpdateForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 
 class Homeview(ListView):
@@ -14,15 +17,26 @@ class Detailedarticle(DetailView):
     model= Post
     template_name='article.html'
 
-class AddPost(CreateView):
+
+class AddPost(CreateView, LoginRequiredMixin):
+    
     model=Post
     template_name="add_post.html"
     #fields=('title', 'author', 'body' )
     #fields=__all__
     form_class=PostForm
+    
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+
+        self.object.save()
+        return super().form_valid(form) 
     def get_success_url(self):
         # Use reverse to dynamically generate the URL with the book ID
         return reverse('articleview', kwargs={'pk': self.object.pk})
+    
+
 class Updatearticlepost(UpdateView):
     model=Post
     template_name="updatepost.html"
